23ec3cf34efae9ec5196170c7890562d9afa8b05
pSaini30 <saini1998pri@gmail.com>
2025-02-26 14:28:59 +0530

    RemoveDuplicateFromSortedArray

---
+++ RemoveDuplicateFromSortedArray/Code.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        int removeDuplicates(vector<int>& nums) {
+            int k =0 ;
+             for (int i =0 ; i < nums.size()-1; i++)
+             {
+                if(nums[i]!=nums[i+1])
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+             }
+             nums[k] = nums[nums.size()-1];
+             return k+1;
+        }
+};
